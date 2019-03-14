afe3c2693c959476e76bd3db60bbd1d3af7191a0
kiburei <gooddeeds17@gmil.com>
2019-03-14 16:41:40 +0300

    day one of the rest

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :addresses
-  devise_for :shops
+  devise_for :shops, controllers: { registrations: "registrations" }
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
