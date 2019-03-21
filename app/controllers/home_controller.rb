775219b80288be1e7bebb53b09fe9c67e687bf9b
kiburei <gooddeeds17@gmil.com>
2019-03-21 10:52:09 +0300

    clean commit before creating cart

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @shop = Shop.last
     @top_products = Product.order(rating: :desc).limit(4)
     @latest_products = Product.last(4)
   end
