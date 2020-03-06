b22f00c2afff13d5e398151f386ee26821520962
Joan Njuguna <njugunajoan35@gmail.com>
2020-03-06 15:40:39 +0300

    update wishlist routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :products
   resources :order_items
   resources :orders
+  resources :wishlist
   devise_for :shops, controllers: { registrations: "registrations" }
   root 'home#index'
   get '/add_to_cart' => 'shopping_cart#add_to_cart', as: :add_to_cart
@@ -13,5 +14,7 @@ Rails.application.routes.draw do
   get '/minusItem/:order_item_id' => 'shopping_cart#minus_item', as: :minus_item
   get 'payment', to: 'orders#payment'
   get 'confirmations', to: 'orders#confirmations'
+  get '/wishlist/' =>'wishlist#wishlist'
+  get '/remove_from_list/' => 'wishlist#remove_from_wishlist', as: :remove_from_list
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
